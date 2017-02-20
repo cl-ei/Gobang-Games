82a5b25be01b2c8d95bfac74814296f4c4ced4b9
Caoliang <i@caoliang.net>
2017-02-20 14:01:05 +0800

    reduce code

--- calcul/tree_udf/tr.py
+++ calcul/tree_udf/tr.py
@@ -1,22 +1,28 @@
-class Tree():
-    def __init__(self, score = 0, pos = (-1,-1)):
+# -*- coding: utf-8 -*-
+
+
+class Tree(object):
+    def __init__(self, score, pos):
         self.score = score
         self.pos = pos
         self.ptr_sum = 0
         self.ptr = []
-    def add(self,pointer):
-        self.ptr_sum = self.ptr_sum + 1
-        self.ptr = self.ptr + [pointer]
+
+    def add(self, pointer):
+        self.ptr_sum += 1
+        self.ptr += [pointer]
+
 
 def cal_sub(tr):
     score = tr.score
 
-    if tr.ptr_sum == 0 :
+    if tr.ptr_sum == 0:
         return score
     else:
         for i in range(tr.ptr_sum):
             score = score + cal_sub(tr.ptr[i]) 
     return score
+
 
 def cal_pos(tr):
     if tr.ptr_sum <= 1:
@@ -24,7 +30,7 @@ def cal_pos(tr):
     else:
         old_score = tr.ptr[0].score
         best_pos = tr.ptr[0].pos
-        for i in range(1,tr.ptr_sum):
+        for i in range(1, tr.ptr_sum):
             new_score = cal_sub(tr.ptr[i])
             if new_score > old_score:
                 best_pos = tr.ptr[i].pos
