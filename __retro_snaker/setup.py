d86bde15bcaebe0e7078ddfe5ec3c6c0b0702a0a
Caoliang <i@caoliang.net>
2017-09-26 10:56:23 +0800

    pep - 8

--- __retro_snaker/setup.py
+++ __retro_snaker/setup.py
@@ -2,23 +2,36 @@ import sys
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
-build_exe_options = {"packages": ["os"], "excludes": ["tkinter"]}
+build_exe_options = {
+    "packages": ["os"],
+    "excludes": ["tkinter"]
+}
 
 # GUI applications require a different base on Windows (the default is for a
 # console application).
 base = 'Win32GUI'
 
-options = {
-   'build_exe':{
-   'include_files':['cl.ttf','head.png']
-   }   
-}
-executables = [
-   Executable('game.py',base=base,icon='snake.ico')
-]
-setup(  name = "clsnake",
-        version = "0.1",
-        description = "a tiny game",
-        options = options,
-        executables = executables
-      )
+
+setup(
+    name="retrosnaker",
+    author="caoliang",
+    author_email="i@caoliang.net",
+    version="1.0.0",
+    description="a tiny game.",
+    install_requires=[
+        "pygames",
+    ],
+    options={
+        "build_exe": {
+            "include_files": ["cl.ttf", "head.png"]
+        }
+    },
+    executables=[
+        Executable('main.py', base=base, icon='snake.ico')
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
