3df121adbc412b6905122df797f02bfc179c449f
Caoliang <i@caoliang.net>
2017-09-28 13:38:50 +0800

    release retro snake.

--- __retro_snaker/main.py
+++ __retro_snaker/main.py
@@ -6,19 +6,8 @@ from pygame.locals import QUIT, KEYUP, K_UP, K_DOWN, K_LEFT, K_RIGHT
 direction_choice = ("L", "R", "U", "D")
 
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self, start_x, start_y, width, height):
-        super(Player, self).__init__()
-
-        self.player_img_path = "head.png"
-        self.image = pygame.transform.scale(pygame.image.load(self.player_img_path), (width, height))
-        self.rect = self.image.get_rect()
-        self.rect.x = start_x
-        self.rect.y = start_y
-
-
 class Snake(object):
-    def __init__(self, windows_size, caption=None, icon=None, fps=20):
+    def __init__(self, windows_size, caption=None, icon=None, fps=50):
         self.windows_size = windows_size
 
         pygame.init()
@@ -29,19 +18,27 @@ class Snake(object):
         if icon is not None:
             pygame.display.set_icon(icon)
 
+        self.head_img_width = int(self.windows_size[0] * 0.25)
+        self.head_img = pygame.transform.scale(
+            pygame.image.load("./head.png").convert_alpha(),
+            (self.head_img_width, self.head_img_width)
+        )
+
+        self.apple_img = pygame.transform.scale(
+            pygame.image.load("./apple.png").convert_alpha(),
+            (50, 50)
+        )
+
         self.fps = fps
-
-        self.direction = random.choice(direction_choice)
-        self.score = 0
+        self.speed = 1
         self.time_tick = 0
-        self.dir_need_frash = False
-        self.length = 2
 
-        self.positon = [[5, 5], [4, 5]]
-        self.position_display = [0, 0, 50, 50]
-        self.position_obs = [3, 6]
-        self.position_tlg = [0, 0]
         self.border = (int(windows_size[0]/50), int(windows_size[1]/50))
+
+        self.last_dir = None
+        self.direction = None
+        self.positon = None
+        self.position_obs = None
 
     def draw_rect(self, color, rect, width=0):
         pygame.draw.rect(self.screen, color, rect, width)
@@ -59,6 +56,8 @@ class Snake(object):
         game_font_small = pygame.font.Font("cl.ttf", 18)
         game_start_image = game_font.render("WELCOME TO PLAY CL'S SNAKE!", True, (222, 100, 70))
         game_start_image_2 = game_font_small.render("PRESS ANY BUTTON TO START -->", True, (222, 100, 70))
+
+        self.screen.fill((30, 30, 30))
         self.screen.blit(
             game_start_image,
             (self.windows_size[0] * 0.18, self.windows_size[1] * 0.7)
@@ -68,9 +67,13 @@ class Snake(object):
             (self.windows_size[0] * 0.18 + 65, self.windows_size[1] * 0.7 + 45)
         )
 
-        cl_player = Player(280, 120, 150, 150)
-        display_img = pygame.sprite.RenderPlain(cl_player)
-        display_img.draw(self.screen)
+        self.screen.blit(
+            self.head_img,
+            (
+                (self.windows_size[0] - self.head_img_width) / 2,
+                self.windows_size[1] * 0.2
+            )
+        )
         self.update_display()
 
         while True:
@@ -95,7 +98,7 @@ class Snake(object):
             source=game_over_img,
             dest=(windows[0] * 0.37, windows[1] * 0.4)
         )
-        score_img = game_over_font_small.render("SCORE : %s" % self.score, True, (222, 100, 80))
+        score_img = game_over_font_small.render("SCORE : %s" % (len(self.positon) - 2), True, (222, 100, 80))
         screen.blit(
             source=score_img,
             dest=(windows[0] * 0.4, windows[1] * 0.4 + 70)
@@ -107,29 +110,8 @@ class Snake(object):
             for event in pygame.event.get():
                 if event.type == QUIT:
                     sys.exit(0)
-
-    def update_position(self):
-        next_position_map = {
-            "L": [self.positon[0][0] - 1, self.positon[0][1]],
-            "R": [self.positon[0][0] + 1, self.positon[0][1]],
-            "U": [self.positon[0][0], self.positon[0][1] - 1],
-            "D": [self.positon[0][0], self.positon[0][1] + 1],
-        }
-        self.positon.insert(0, next_position_map.get(self.direction))
-        self.position_tlg = self.positon.pop(self.length)
-
-    def add_tlg(self):
-        self.positon.insert(self.length, self.position_tlg)
-        self.length += 1
-
-    def update_obs(self):
-        on_snake = True
-        while on_snake:
-            self.position_obs = [random.randint(0, self.border[0]-1), random.randint(0, self.border[1]-1)]
-            if self.positon.count(self.position_obs) == 1:
-                on_snake = True
-            else:
-                on_snake = False
+                elif event.type == KEYUP:
+                    return
 
     def confilct(self):
         return_data = 3
@@ -144,7 +126,7 @@ class Snake(object):
         return return_data
 
     def parse_event(self, event):
-        if event.type != KEYUP or not self.dir_need_frash:
+        if event.type != KEYUP:
             return
 
         forbidden_key_map = {
@@ -160,12 +142,80 @@ class Snake(object):
             K_RIGHT: "R",
         }
         event_key_dir = switch_key_map.get(event.key)
-        if event_key_dir and forbidden_key_map.get(event_key_dir) != self.direction:
-            self.dir_need_frash = False
+        if event_key_dir and forbidden_key_map.get(event_key_dir) != self.last_dir:
             self.direction = event_key_dir
 
-    def update(self):
-        self.screen.fill((30, 30, 30))
+    def take_one_step(self):
+        """return: is game over."""
+        result = False
+        if self.time_tick * self.speed >= self.fps:
+            self.time_tick = 0
+            result = self.update_position()
+        return result
+
+    def update_position(self):
+        self.last_dir = self.direction
+        self.speed = 1 + (len(self.positon) - 2) * 0.1
+
+        next_position_map = {
+            "L": [self.positon[0][0] - 1, self.positon[0][1]],
+            "R": [self.positon[0][0] + 1, self.positon[0][1]],
+            "U": [self.positon[0][0], self.positon[0][1] - 1],
+            "D": [self.positon[0][0], self.positon[0][1] + 1],
+        }
+        x, y = next_position_map.get(self.direction)
+        if [x, y] in self.positon:
+            # eat self.
+            return True
+
+        if x < 0 or y < 0 or x > self.border[0] - 1 or y > self.border[1] - 1:
+            # over border.
+            return True
+
+        self.positon.insert(0, [x, y])
+        if x == self.position_obs[0] and y == self.position_obs[1]:
+            # eat apple.
+            self.gen_obs_postion()
+            return False
+        else:
+            # nothing happend
+            self.positon.pop()
+            return False
+
+    def gen_obs_postion(self):
+        while True:
+            x, y = random.randint(0, self.border[0] - 1), random.randint(0, self.border[1] - 1)
+            if [x, y] not in self.positon:
+                break
+
+        self.position_obs = [x, y]
+
+    def draw_table(self):
+        for i in range(len(self.positon)):
+            self.draw_rect(
+                color=(255, 255, 255),
+                rect=(self.positon[i][0] * 50 + 5, self.positon[i][1] * 50 + 5, 40, 40)
+            )
+
+        # snake head
+        self.draw_circle(
+            color=(100, 0, 0),
+            pos=(self.positon[0][0] * 50 + 25, self.positon[0][1] * 50 + 25),
+            radius=10
+        )
+
+        # apple
+        self.screen.blit(
+            self.apple_img,
+            (self.position_obs[0] * 50, self.position_obs[1] * 50)
+        )
+
+    def gaming(self):
+        self.last_dir = "R"
+        self.direction = "R"
+        self.positon = [[5, 5], [4, 5]]
+        self.position_obs = [3, 6]
+
         while True:
             for event in pygame.event.get():
                 if event.type == QUIT:
@@ -173,19 +223,13 @@ class Snake(object):
                 else:
                     self.parse_event(event=event)
 
-            for i in range(self.length):
-                self.draw_rect(
-                    color=(255, 255, 255),
-                    rect=(self.positon[i][0]*50 + 5, self.positon[i][1]*50 + 5, 40, 40)
-                )
-                self.draw_circle(
-                    color=(100, 0, 0),
-                    pos=(self.positon[0][0]*50+25, self.positon[0][1]*50+25),
-                    radius=10
-                )
-            self.draw_rect(color=(200, 0, 0), rect=(self.position_obs[0]*50, self.position_obs[1]*50, 50, 50))
-
+            self.screen.fill((30, 30, 30))
+            game_over = self.take_one_step()
+            self.draw_table()
             self.update_display()
+
+            if game_over:
+                return False
 
 
 def main():
@@ -196,7 +240,7 @@ def main():
 
     while True:
         ratro_snaker.start()
-        ratro_snaker.update()
+        ratro_snaker.gaming()
         ratro_snaker.over()
 
 
